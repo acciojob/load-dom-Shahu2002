ff7440f4790927de090d5df63a0cf58e1e3de560
Shahu Patil <139709118+Shahu2002@users.noreply.github.com>
2023-09-29 16:12:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 //your JS code here. If required.
-let str = document.createElement("h1");
-str.innerHTML= "DOM load success";
-console.log(str);
+document.addEventListener("DOMContentLoaded", function() {
+        // Get the body element
+        var body = document.body;
+
+        // Remove any existing content in the body
+        while (body.firstChild) {
+            body.removeChild(body.firstChild);
+        }
+
+        // Create a new text node with the message
+        var textNode = document.createTextNode("DOM load success");
+
+        // Append the text node to the body
+        body.appendChild(textNode);
+    });
